4c202a8159d87a6e0c99933f7892ee3ba1207e98
Jenrola Jackson <jacksinsthegenius@gmail.com>
2023-11-14 18:09:16 +0100

    feat: add faq section

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,5 +1,47 @@
-const FAQ = () =>{
-    return
-}
+import faqs from "../faqData";
+import { useState } from "react";
 
-export default FAQ
+const FAQ = () => {
+  const [activeTab, setActiveTab] = useState(null);
+
+  const questionsAnswers = faqs.map((item, index) => {
+    return (
+      <div className="question" key={index}>
+        <h3>{item.title}</h3>
+
+        {item.questions.map((item) => {
+          return (
+            <div
+              className="question--wrapper"
+              key={item.id}
+              onClick={() =>
+                setActiveTab((prevId) => (prevId === item.id ? null : item.id))
+              }
+            >
+              <div className="controls">
+                <h5>{item.q}</h5>
+                {activeTab === item.id ? (
+                  <span class="material-symbols-outlined">remove</span>
+                ) : (
+                  <span class="material-symbols-outlined">add</span>
+                )}
+              </div>
+              {activeTab === item.id && <p>{item.a}</p>}
+            </div>
+          );
+        })}
+      </div>
+    );
+  });
+
+  return (
+    <section className="faq__section">
+      <h4>FAQs</h4>
+      <h2>Questions? We have answers!</h2>
+
+      <div className="faqs">{questionsAnswers}</div>
+    </section>
+  );
+};
+
+export default FAQ;
---
+++ src/faqData.js
@@ -0,0 +1,80 @@
+const faqs = [
+  {
+    title: "Getting started with Savewise",
+    questions: [
+      {
+        q: "What is Savewise?",
+        a: "Savewise is a cutting-edge savings platform designed to help you achieve financial freedom.",
+        id: 1,
+      },
+      {
+        q: "How do I get started with Savewise?",
+        a: "Getting started is easy! Simply sign up on our platform, set your financial goals, and let Savewise guide you through the process.",
+        id: 2,
+      },
+      {
+        q: "Is Savewise suitable for beginners in finance?",
+        a: "Yes, Savewise is designed for everyone, from financial novices to experts. Our user-friendly interface and step-by-step guidance make it easy for anyone to start their journey to financial freedom.",
+        id: 3,
+      },
+    ],
+  },
+  {
+    title: "Security and Transparency",
+    questions: [
+      {
+        q: "Are my funds secure with Savewise?",
+        a: "Absolutely. We take your financial security seriously. Savewise employs top-notch encryption and security measures to ensure your funds and personal information are safe and protected.",
+        id: 4,
+      },
+      {
+        q: "Are there any hidden fees with Savewise?",
+        a: "No hidden fees here. Savewise believes in transparency. We clearly outline any fees associated with our services, ensuring you have a clear understanding of your financial commitments.",
+        id: 5,
+      },
+    ],
+  },
+  {
+    title: "Personalised Saving Plans",
+    questions: [
+      {
+        q: "Can I customize my saving plan?",
+        a: "Yes, you can! Savewise understands that everyone's financial journey is unique. Our platform allows you to customize your saving plan based on your specific goals.",
+        id: 6,
+      },
+    ],
+  },
+  {
+    title: "Maximizing Savings and Investments",
+    questions: [
+      {
+        q: " How does Savewise maximize my savings?",
+        a: "Savewise goes beyond simple savings. Our platform offers features like automatic round-ups, budgeting tools, and intelligent investment options to optimize your savings and help your money grow.",
+        id: 7,
+      },
+    ],
+  },
+  {
+    title: "Financial Insights and Tracking",
+    questions: [
+      {
+        q: "Can I track my spending on Savewise?",
+        a: "Absolutely. Our financial insights give you a clear view of your spending habits. Track your expenses, monitor your savings progress, and make informed decisions about your money.",
+        id: 8,
+      },
+    ],
+  },
+  {
+    title: "Contact and Support",
+    questions: [
+      {
+        q: "How do I contact Savewise customer support?",
+        a: "Our dedicated customer support team is here to assist you. You can reach out through our platform's messaging system or via email at support@savewise.com. We're here to help you on your journey to financial success!",
+        id: 9,
+      },
+    ],
+  },
+];
+
+
+export default faqs
